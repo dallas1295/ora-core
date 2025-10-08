3b2670279e040151fc88980c01ba0c2a20749601
Dallas S <adsherman@pm.me>
2025-10-08 14:30:34 +0800

    feat: Added NoChanges error and proper err matching for error response from NoteError from domain.

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,14 @@
 use thiserror::Error;
 
+pub type OraResult<T> = Result<T, OraError>;
+
 #[derive(Debug, Error)]
 pub enum OraError {
+    #[error("no changes to file")]
+    NoChanges,
+
     #[error(transparent)]
-    Note(#[from] crate::domain::NoteError),
+    Note(crate::domain::NoteError),
 
     #[error(transparent)]
     Shelf(#[from] crate::shelf::storage::ShelfError),
@@ -21,4 +26,19 @@ pub enum OraError {
     Other(String),
 }
 
-pub type OraResult<T> = Result<T, OraError>;
+impl From<crate::domain::NoteError> for OraError {
+    fn from(err: crate::domain::NoteError) -> Self {
+        match err {
+            crate::domain::NoteError::NoChanges => OraError::NoChanges,
+            crate::domain::NoteError::InvalidPath => {
+                OraError::Note(crate::domain::NoteError::InvalidPath)
+            }
+            crate::domain::NoteError::InvalidTitle => {
+                OraError::Note(crate::domain::NoteError::InvalidTitle)
+            }
+            crate::domain::NoteError::Io(io_error) => {
+                OraError::Note(crate::domain::NoteError::Io(io_error))
+            }
+        }
+    }
+}
